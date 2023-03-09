746c84fd64b0e4fffde032f05ebfd46b947e4471
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-09 16:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+function letterCombinations(digits) {
+  const mappings = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  };
+  
+  if (digits.length === 0) {
+    return [];
+  }
+  
+  const combinations = [""];
 
-module.exports = letterCombinations;
+  for (let i = 0; i < digits.length; i++) {
+    const letters = mappings[digits[i]];
+    const newCombinations = [];
+
+    for (let j = 0; j < letters.length; j++) {
+      for (let k = 0; k < combinations.length; k++) {
+        newCombinations.push(combinations[k] + letters[j]);
+      }
+    }
+
+    combinations.splice(0, combinations.length, ...newCombinations);
+  }
+
+  return combinations.sort();
+}
